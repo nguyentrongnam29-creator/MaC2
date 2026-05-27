fa989c039bb8dba5609b091d7789cfd8557bdca0
nguyentrongnam29-creator <nguyentrongnam29@gmail.com>
2026-05-27 03:04:54 +0000

    Add default player structure

---
+++ src/data/defaultPlayer.ts
@@ -0,0 +1,31 @@
+export const defaultPlayer = {
+  username: "Cultivator",
+
+  level: 1,
+  exp: 0,
+  maxExp: 100,
+
+  realm: "Luyện Khí Sơ Kỳ",
+
+  stats: {
+    hp: 100,
+    mana: 50,
+    atk: 10,
+    defense: 5,
+  },
+
+  streak: 0,
+
+  gold: 0,
+
+  spiritRoot: {
+    type: "Phàm",
+    expMultiplier: 1,
+    statBonus: {
+      hp: 0,
+      mana: 0,
+      atk: 0,
+      defense: 0,
+    },
+  },
+}
