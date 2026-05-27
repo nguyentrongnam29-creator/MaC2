ea2cb3f1d782ac3a080169ba009ca67f36ec0147
nguyentrongnam29-creator <nguyentrongnam29@gmail.com>
2026-05-27 07:56:35 +0000

    Add enemy realm color UI

--- src/data/enemies.ts
+++ src/data/enemies.ts
@@ -3,17 +3,23 @@ export const enemies = [
     name: "Yêu Lang",
     realm: "Luyện Khí Nhất Trọng",
     maxHp: 100,
+    damage: 5,
+    color: "#9ca3af",
   },
 
   {
     name: "Hắc Xà",
     realm: "Luyện Khí Nhị Trọng",
-    maxHp: 120,
+    maxHp: 140,
+    damage: 10,
+    color: "#22c55e",
   },
 
   {
     name: "Ma Viên",
     realm: "Luyện Khí Tam Trọng",
-    maxHp: 150,
+    maxHp: 180,
+    damage: 15,
+    color: "#ef4444",
   },
 ]
