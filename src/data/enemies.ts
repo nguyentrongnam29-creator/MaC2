066e2ca967aebef1961772ffeff5ac12318dcc80
nguyentrongnam29-creator <nguyentrongnam29@gmail.com>
2026-05-27 07:35:58 +0000

    Add enemy HP combat system

---
+++ src/data/enemies.ts
@@ -0,0 +1,19 @@
+export const enemies = [
+  {
+    name: "Yêu Lang",
+    realm: "Luyện Khí Nhất Trọng",
+    maxHp: 100,
+  },
+
+  {
+    name: "Hắc Xà",
+    realm: "Luyện Khí Nhị Trọng",
+    maxHp: 120,
+  },
+
+  {
+    name: "Ma Viên",
+    realm: "Luyện Khí Tam Trọng",
+    maxHp: 150,
+  },
+]
