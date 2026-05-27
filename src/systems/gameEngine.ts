887aa798354f169cecef7aa1a4d0072aff24d3cd
nguyentrongnam29-creator <nguyentrongnam29@gmail.com>
2026-05-27 07:11:35 +0000

    Prevent repeated quiz questions

--- src/systems/gameEngine.ts
+++ src/systems/gameEngine.ts
@@ -1,8 +1,40 @@
 import { gainExp } from "./expSystem"
 import { checkLevelUp } from "./levelSystem"
 
-export function handleCorrectAnswer() {
-  gainExp(10)
+import { getPlayer } from "./playerSystem"
 
-  return checkLevelUp()
+export function handleCorrectAnswer() {
+  const player = getPlayer()
+
+  const newStreak =
+    player.streak + 1
+
+  let expGain = 10
+
+  if (newStreak >= 3) {
+    expGain = 20
+  } else if (newStreak >= 2) {
+    expGain = 15
+  }
+
+  gainExp(expGain)
+
+  const updatedPlayer =
+    checkLevelUp()
+
+  updatedPlayer.streak =
+    newStreak
+
+  return {
+    ...updatedPlayer,
+    gainedExp: expGain,
+  }
+}
+
+export function handleWrongAnswer() {
+  const player = getPlayer()
+
+  player.streak = 0
+
+  return player
 }
