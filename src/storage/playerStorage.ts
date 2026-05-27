f1e904f38729c5c0b0b24946d86c3ce5ea5f5970
nguyentrongnam29-creator <nguyentrongnam29@gmail.com>
2026-05-27 03:14:18 +0000

    Add player local storage system

---
+++ src/storage/playerStorage.ts
@@ -0,0 +1,30 @@
+import { defaultPlayer } from "../data/defaultPlayer"
+
+const PLAYER_STORAGE_KEY = "mac2_player"
+
+export function savePlayer(player: any) {
+  localStorage.setItem(
+    PLAYER_STORAGE_KEY,
+    JSON.stringify(player)
+  )
+}
+
+export function loadPlayer() {
+  const savedPlayer = localStorage.getItem(
+    PLAYER_STORAGE_KEY
+  )
+
+  if (!savedPlayer) {
+    return defaultPlayer
+  }
+
+  return JSON.parse(savedPlayer)
+}
+
+export function resetPlayer() {
+  localStorage.removeItem(
+    PLAYER_STORAGE_KEY
+  )
+
+  return defaultPlayer
+}
